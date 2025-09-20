fbedaf68dae6921dabb7e94d5c39e13abc7fee8e
priya-066 <priyab0966@gmail.com>
2025-09-20 09:46:13 +0530

    20-09-2025 Customer Page

--- delivery/models.py
+++ delivery/models.py
@@ -13,3 +13,11 @@ class Restaurant(models.Model):
     picture = models.URLField(max_length=500,default="https://static.vecteezy.com/system/resources/previews/052/792/818/non_2x/restaurant-logo-design-vector.jpg")
     cuisine = models.CharField(max_length=500)
     rating = models.FloatField()
+
+class Item(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE, related_name = "items")
+    name = models.CharField(max_length = 20)
+    picture = models.URLField(max_length = 200, default="https://cdn-icons-png.flaticon.com/512/1147/1147856.png")
+    description = models.CharField(max_length = 200)
+    price = models.FloatField()
+    is_veg = models.BooleanField(default = True)
--- delivery/views.py
+++ delivery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Customer
 from .models import Restaurant
+from .models import Item
 
 # Create your views here.
 def login(request):
@@ -40,15 +41,22 @@ def signin1(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-    try:
-        Customer.objects.get(username = username,password = password)
-        if username == "admin":
-            return render (request, "admin_home.html")
-        else:
-            return render(request,"customer_home.html")
-        
-    except Customer.DoesNotExist:
-        return render(request, "fail.html")
+        try:
+            Customer.objects.get(username=username, password=password)
+            if username == "admin":
+                return render(request, "admin_home.html")
+            else:
+                restaurants = Restaurant.objects.all()
+                return render(
+                    request,
+                    "customer_home.html",
+                    {"restaurants": restaurants, "username": username}
+                )
+        except Customer.DoesNotExist:
+            return render(request, "fail.html")
+
+    return render(request, "sigin.html")
+
     
 
 def add_restaurant_page(request):
@@ -102,8 +110,43 @@ def delete_restaurant(request, restaurant_id):
     if request.method == "POST":
         restaurant.delete()
         return redirect("open_show_restaurant")  # make sure this view exists!
-
-
-
     
+
+def open_update_menu(request, restaurant_id):
+    restaurant = Restaurant.objects.get( id=restaurant_id)
+    #itemList = Item.objects.all()
+    itemList = restaurant.items.all()
+    return render(request, 'update_menu.html', {"itemList": itemList, "restaurant": restaurant})
+
+
+def update_menu(request,restaurant_id ):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        is_veg = request.POST.get('is_veg') == 'on'
+        picture = request.POST.get('picture')
+
+        
+        Item.objects.create(
+            restaurant=restaurant,
+            name=name,
+            description=description,
+            price=price,
+            is_veg=is_veg,
+            picture=picture
+        )
+        return render(request, 'admin_home.html')
+    
+
+def view_menu(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    itemList = restaurant.items.all()
+    return render(
+        request,
+        'customer_menu.html',
+        {"itemList": itemList, "restaurant": restaurant}
+    )
         
